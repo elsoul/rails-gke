ad6e68713235dd608209ace415d571b8b4d3fdeb
FUMI-POPPIN <fumitake.kawasaki@el-soul.com>
2020-08-11 23:17:58 -0700

    add mothod list

--- lib/rails/gke.rb
+++ lib/rails/gke.rb
@@ -7,12 +7,63 @@ module Rails
       attr_accessor :configuration
     end
 
+    def create_cluster
+      network = Rails::Gke.configuration.network
+      sub_network = Rails::Gke.configuration.sub_network
+      machine_type = Rails::Gke.configuration.machine_type
+      system("gcloud container clusters create graphql-api-cluster --region asia-northeast1 --num-nodes 1 \
+        --machine-type #{machine_type} --enable-autorepair --enable-ip-alias --network #{network} --subnetwork #{sub_network}")
+    end
+
+    def create_namespace
+    end
+
+    def create_ip
+    end
+
+    def apply_deployment
+    end
+
+    def apply_secret
+    end
+
+    def apply_service
+    end
+
+    def apply_ingress
+    end
+
+    def delete_deployment
+    end
+
+    def delete_secret
+    end
+
+    def delete_service
+    end
+
+    def delete_ingress
+    end
+
+    def update_container
+    end
+
+    def get_pods
+    end
+
+    def get_svc
+    end
+
+    def get_ingress
+    end
+
+    def run_test
+    end
+
     def self.configure
       self.configuration ||= Configuration.new
       yield(configuration)
     end
-
-    private
 
     class Configuration
       attr_accessor :project_id, :app, :network, :sub_network, :machine_type, :namespace, :global_ip
--- spec/rails/gke_spec.rb
+++ spec/rails/gke_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe Rails::Gke do
     it "Should be able to set configuration" do
       Rails::Gke.configure do |config|
         config.project_id = "abc123"
+        config.app = "abc123"
+        config.network = "abc123"
+        config.sub_network = "abc123"
+        config.machine_type = "abc123"
+        config.namespace = "abc123"
+        config.global_ip = "abc123"
       end
 
       expect(Rails::Gke.configuration.project_id).to eq "abc123"
