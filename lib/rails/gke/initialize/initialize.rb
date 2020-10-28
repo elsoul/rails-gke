9a48887ad078feb8160854b4ce6a715f2444f2ae
FUMI-POPPIN <fumitake.kawasaki@el-soul.com>
2020-10-28 11:36:06 +0100

    update initial config

--- lib/rails/gke/initialize/initialize.rb
+++ lib/rails/gke/initialize/initialize.rb
@@ -9,12 +9,12 @@ module Rails
             Rails::Gke.configure do |config|
               config.project_id = "PROJECT_ID"
               config.app = "APP-name"
-              config.network = "NETWORK"
-              config.machine_type = "MACHINE_TYPE"
-              config.zone = "ZONE"
+              config.network = "default"
+              config.machine_type = "custom-1-6656"
+              config.zone = "asia-northeast1"
               config.domain = "DOMAIN"
               config.channel = "stable"
-              config.google_application_credentials = "your-GOOGLE_APPLICATION_CREDENTIALS"
+              config.google_application_credentials = "GOOGLE_APPLICATION_CREDENTIALS"
             end
           EOS
         end
